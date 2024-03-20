690350d246a3ef8e39bfc5301ccb88d0acfa478f
matze2402 <145361419+matze2402@users.noreply.github.com>
2024-03-20 08:14:12 +0100

    Update update_sidebar.py

--- .github/scripts/update_sidebar.py
+++ .github/scripts/update_sidebar.py
@@ -1,10 +1,11 @@
 import os
+import yaml
 
 # Path to the folder containing the files for the sidebar
-folder_path = "./pages/Testpurpose"  # Update this with your folder path
+folder_path = './pages/Testpurpose'  # Update this with your folder path
 
-# Path to the markdown file where the sidebar content will be written
-sidebar_file_path = "./_data/sidebars/mydoc_sidebar.yml"  # Update this with your desired sidebar file path
+# Path to the YAML file where the sidebar content will be written
+sidebar_file_path = './_data/sidebars/mydoc_sidebar.yml'  # Update this with your desired sidebar file path
 
 def generate_sidebar():
     # Fetch the list of files in the folder
@@ -13,10 +14,12 @@ def generate_sidebar():
     # Filter out directories, if any
     files = [f for f in files if os.path.isfile(os.path.join(folder_path, f))]
 
-    # Write the sidebar content to the markdown file
+    # Convert the list of files to a dictionary suitable for YAML
+    sidebar_content = [{'title': file, 'url': file} for file in files]
+
+    # Write the sidebar content to the YAML file
     with open(sidebar_file_path, 'w') as sidebar_file:
-        for file in files:
-            sidebar_file.write(f'* [{file}]({file})\n')
+        yaml.dump(sidebar_content, sidebar_file, default_flow_style=False)
 
     print(f"Sidebar content generated and saved to '{sidebar_file_path}'")
 
